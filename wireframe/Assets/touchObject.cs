9d1c6a80410db1e825f65bef94dc620afc721c73
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2019-12-21 16:12:54 +0900

    wireframeAction

---
+++ wireframe/Assets/touchObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchObject : MonoBehaviour
+{
+    public Material wireframeMaterial;
+    public Material originMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.gameObject.GetComponent<wireframe>().enabled = true;
+        other.gameObject.GetComponent<Renderer>().material = wireframeMaterial;
+        other.transform.Find("Capsule").GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Renderer>().material = originMaterial;
+        other.gameObject.GetComponent<wireframe>().enabled = false;
+        other.transform.Find("Capsule").GetComponent<MeshRenderer>().enabled = true;
+    }
+}
+    
